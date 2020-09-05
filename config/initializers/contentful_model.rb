128ba26f63dd6694d76d8a1d6e87810dbf6f8b5a
Gorgolevskiy Pavel <gorgolevsky@gmail.com>
2020-09-05 09:44:12 +0300

    Added gem contentful_model

---
+++ config/initializers/contentful_model.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+ContentfulModel.configure do |config|
+  config.access_token = ENV.fetch('CONTENTFUL_DELIVERY_API_TOKEN')
+  # config.preview_access_token = "your preview token in here" # Optional - required if you want to use the preview API
+  # config.management_token = "your management token in here" # Optional - required if you want to update or create content
+  config.space = ENV.fetch('CONTENTFUL_DELIVERY_API_SPACE')
+  # config.environment = "master" # Optional - defaults to 'master'
+  config.default_locale = 'en-US' # Optional - defaults to 'en-US'
+  config.options = { # Optional
+    # Extra options to send to the Contentful::Client and Contentful::Management::Client
+    # See https://github.com/contentful/contentful.rb#configuration
+
+    # Optional:
+    # Use `delivery_api` and `management_api` keys to limit to what API the settings
+    # will apply. Useful because Delivery API is usually visitor facing, while Management
+    # is used in background tasks that can run much longer.
+    delivery_api: {
+      timeout_read: 6,
+      timeout_write: 6,
+      timeout_connect: 6
+    }
+    # management_api: {
+    #   timeout_read: 100
+    # }
+  }
+end
